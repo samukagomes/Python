d3654b39817d5afa90a891631ca35acc0b138468
Samuel Gomes <samuel.jesus3@etec.sp.gov.br>
2023-08-03 20:53:27 -0300

    Terminei as funções do banco
    Terminei a criação do banco e das classes para a sua manipulação. Próximo passo, criar as telas e suas respectivas logicas

--- Projetos/Sites/Flask/Login/home.py
+++ Projetos/Sites/Flask/Login/home.py
@@ -3,12 +3,17 @@ from flask import render_template
 
 app = Flask(__name__)
 
-# @app.route('/')
-# dwf index(/)
+@app.route('/')
+def index():
+    return render_template('index.html')        
 
 @app.route('/login/')
 def login():
     return render_template('login.html')
+
+app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
 
 @app.route('/user/<user>')
 def usuario(user):

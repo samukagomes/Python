47956ecb52110a2b801062cb4c455a06744bccc8
Samuel Gomes <Samuka.gj2@outlook.com>
2023-11-09 21:34:01 -0300

    tudo tem um começo

--- Projetos/Web scraping/Analise de noticias/scripts/main.py
+++ Projetos/Web scraping/Analise de noticias/scripts/main.py
@@ -1,4 +1,11 @@
 from bs4 import BeautifulSoup as bf
+import httpx
 
-html = 'https://g1.globo.com/'
-soup = bf()
+endpoint = input('escreva o tema da noticia que deseja pesquisar: ')
+response = httpx.get('https://g1.globo.com/busca/?q='+endpoint)
+soup = bf(response.text, 'html.parser')
+
+link = soup.find_all('a')
+
+for i in range(0,10):
+    print(str(i)+" "+link[i].get('href'))

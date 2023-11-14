ed11a74abb411d980dbc8bed19f00de1a4e05edc
Samuel Gomes <Samuka.gj2@outlook.com>
2023-11-13 21:36:47 -0300

    estruturei em forma de classe e já consigo buscar links do g1

--- Projetos/Web scraping/Analise de noticias/scripts/main.py
+++ Projetos/Web scraping/Analise de noticias/scripts/main.py
@@ -1,11 +1,27 @@
 from bs4 import BeautifulSoup as bf
 import httpx
 
+class reportagem():
+    def __init__(self, endpoint):
+        self.endpoint = endpoint 
+
+    def pesquisa(self):
+        response = httpx.get('https://g1.globo.com/busca/?q='+self.endpoint)
+        self.soup_g1 = bf(response.text, 'html.parser')
+
+        response = httpx.get('https://g1.globo.com/busca/?q='+self.endpoint)
+        self.soup_uol = bf(response.text, 'html.parser')
+
+    def g1(self):
+        div = self.soup_g1.find_all('div', 'widget--info__text-container', limit=2)
+
+        link=div[0].find('a')
+
+        print(link.get('href'))
+
+
 endpoint = input('escreva o tema da noticia que deseja pesquisar: ')
-response = httpx.get('https://g1.globo.com/busca/?q='+endpoint)
-soup = bf(response.text, 'html.parser')
 
-link = soup.find_all('a')
-
-for i in range(0,10):
-    print(str(i)+" "+link[i].get('href'))
+reportagem = reportagem(endpoint)
+reportagem.pesquisa()
+reportagem.g1()
